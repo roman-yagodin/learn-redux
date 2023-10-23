f867dcc508e3fe501bf83bccac0f3bc589921681
roman-yagodin <roman.yagodin@gmail.com>
2023-10-23 11:37:34 +0300

    Перенос старого проекта в шаблон create-react-app

---
+++ src/components/SearchForm.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class SearchForm extends React.Component{
+ 
+ constructor(props){
+     super(props);
+     this.state = {  };
+     this.handleSubmit = this.handleSubmit.bind(this);
+     this.handleChange = this.handleChange.bind(this);
+  }
+ 
+ componentDidMount(){
+
+ }
+ 
+
+  handleSubmit(ev){
+
+  }
+  handleChange(ev){
+
+  }
+ 
+  render(){
+   return <form action="" onSubmit={this.handleSubmit} className="searchform">
+ <h3>Поиск книги</h3>     
+ <div><label>Название</label> <input type="text" name="title"  onChange={this.handleChange} value={this.state.title} ref={this.setRefInputTitle} autoComplete="off" /><input type="submit" value="Искать" className="btn btn-success"/></div>
+</form>;
+
+   
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import dataBook from './data/data.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App dataBook={dataBook} />
   </React.StrictMode>
 );
 
